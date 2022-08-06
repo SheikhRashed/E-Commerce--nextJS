2180299854e6a5477f0e48520a1c87604e528cd9
SheikhRashed <devrashed06@gmail.com>
2022-08-06 14:16:27 +0600

    update: full width sidebar

--- UI/Components/Sidebar/Sidebar.js
+++ UI/Components/Sidebar/Sidebar.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Disclosure } from '@headlessui/react'
+import { RadioGroup } from '@headlessui/react'
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai"
 import styles from "./sidebar.module.css"
 
 export default function Sidebar() {
+
+  const [color, setColor] = useState('black')
+
+
   return (
     <div className={styles.sidebar}>
 
-      <Disclosure >
+      {/* Category  */}
+      <Disclosure defaultOpen="true" >
         {({ open }) => (
           <>
             <Disclosure.Button className={styles.accordionHeader}>
@@ -18,41 +24,98 @@ export default function Sidebar() {
               }
 
             </Disclosure.Button>
-            <Disclosure.Panel className={styles.accordionBody}>
-              <ul className={styles.productItems}>
-                <li>
-                  <p>Product Name</p>
-                </li>
-                <li>
-                  <p>Product Name</p>
-                </li>
-                <li>
-                  <p>Product Name</p>
-                </li>
-                <li>
-                  <p>Product Name</p>
-                </li>
-              </ul>
+            <Disclosure.Panel>
+              <div className={styles.accordionBody}>
+                <ul className={styles.productItems}>
+                  <li>
+                    <p>Product Name</p>
+                  </li>
+                  <li>
+                    <p>Product Name</p>
+                  </li>
+                  <li>
+                    <p>Product Name</p>
+                  </li>
+                  <li>
+                    <p>Product Name</p>
+                  </li>
+                </ul>
+              </div>
             </Disclosure.Panel>
           </>
         )}
       </Disclosure>
+
+      {/* Price  */}
       <Disclosure>
         {({ open }) => (
           <>
             <Disclosure.Button className={styles.accordionHeader}>
               <span className={styles.accordionTitle}>Price</span>
-
               {
                 open ? <AiOutlineMinus className={styles.accordionIcon} /> : <AiOutlinePlus className={styles.accordionIcon} />
               }
-
             </Disclosure.Button>
             <Disclosure.Panel>
+              <div className={styles.accordionBody}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam est eveniet quaerat iusto pariatur vero maiores, expedita facilis excepturi aliquam, blanditiis commodi. Est, dignissimos iste exercitationem impedit veritatis labore ex? </div>
             </Disclosure.Panel>
           </>
         )}
       </Disclosure>
+
+      {/* Choose Colors  */}
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className={styles.accordionHeader}>
+              <span className={styles.accordionTitle}>Colors</span>
+              {
+                open ? <AiOutlineMinus className={styles.accordionIcon} /> : <AiOutlinePlus className={styles.accordionIcon} />
+              }
+            </Disclosure.Button>
+            <Disclosure.Panel>
+              <div className={styles.acacordionBody}>
+                <RadioGroup value={color} onChange={setColor} className={styles.colorsWrapper}>
+                  <RadioGroup.Option value="black">
+                    {({ checked }) => (
+                      <span className={`${styles.colorBox} ${checked && styles.checkedBox}   ${styles.black}`}></span>
+                    )}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value="blue">
+                    {({ checked }) => (
+                      <span className={`${styles.colorBox} ${checked && styles.checkedBox}   ${styles.blue}`}></span>
+                    )}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value="red">
+                    {({ checked }) => (
+                      <span className={`${styles.colorBox} ${checked && styles.checkedBox}   ${styles.red}`}></span>
+                    )}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value="yellow">
+                    {({ checked }) => (
+                      <span className={`${styles.colorBox} ${checked && styles.checkedBox}   ${styles.yellow}`}></span>
+                    )}
+                  </RadioGroup.Option>
+                </RadioGroup>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+
+      {/* Custom Block */}
+
+      <div className={styles.customBlock}>
+        <h2>Block Title</h2>
+        <span>Block Subtitle</span>
+        <blockquote>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident nihil eaque, praesentium maxime laboriosam veritatis ad cum recusandae voluptate repellendus blanditiis magnam rem quisquam consequuntur minima. Earum accusamus enim magnam!
+          </p>
+        </blockquote>
+      </div>
+
     </div>
   )
 }
