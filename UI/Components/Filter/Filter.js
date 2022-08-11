9ad9049f7094f788e0f51686f72e2b7ae1e554be
SheikhRashed <devrashed06@gmail.com>
2022-08-11 17:44:41 +0600

    update: api integrated on filter

--- UI/Components/Filter/Filter.js
+++ UI/Components/Filter/Filter.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Listbox } from '@headlessui/react'
 import { Form } from "react-bootstrap"
 import styles from "./filter.module.css"
 const showData = [
--- UI/Screens/Category/CategoryScreen.js
+++ UI/Screens/Category/CategoryScreen.js
@@ -1,25 +1,37 @@
 import React, { useState, useEffect } from "react";
-
-import { Container, Row, Col } from "react-bootstrap";
+import { Listbox } from '@headlessui/react'
+import { Container, Row, Col, Form } from "react-bootstrap";
 import sortData from "../../../data/sort-data.json"
+import { AiOutlineSortAscending } from "react-icons/ai"
 import ProductCard from "../../Components/ProductCard/ProductCard";
-import Filter from "../../Components/Filter/Filter";
-
 import styles from "./category.module.css"
 
 
-
 export default function CategoryScreen() {
+  const [search, setSearch] = useState("");
+  const [limit, setLimit] = useState(5);
+  const [asc, setAsc] = useState(false);
+  const [sort, setSort] = useState(sortData[0])
+  const [productList, setProductList] = useState([]);
 
-  const [productList, setProductList] = useState([])
 
-  async function fetchProduct() {
+  function handleSearch(e) {
+    setSearch(e.target.value)
+  }
+
+  function handleLimit(e) {
+    setLimit(e.target.value)
+  }
+  const handleAsc = () => setAsc(!asc)
+
+  async function fetchProduct(search, limit, asc) {
     try {
-      const response = await fetch("https://server.buniyadi.craftedsys.com/api/product?limit=16&resolvePrimaryCategory=1");
+      const response = await fetch(`https://server.buniyadi.craftedsys.com/api/product?search=${search}&limit=${limit}&resolvePrimaryCategory=1&sortOrder=${asc ? 1 : -1}`);
 
       if (response.ok) {
         const jsonResponse = await response.json()
         setProductList(jsonResponse.data)
+
       }
     } catch (err) {
       console.log(err.message)
@@ -27,8 +39,13 @@ export default function CategoryScreen() {
   }
 
   useEffect(() => {
-    fetchProduct()
-  }, [])
+    fetchProduct(search, limit, asc)
+  }, [search, limit, asc])
+
+
+
+
+
 
   return (
     <>
@@ -36,7 +53,24 @@ export default function CategoryScreen() {
       <div className={styles.productList}>
         <Container>
           <Row>
-            <Filter sortingData={sortData} />
+            {/* <Filter sortingData={sortData} /> */}
+            <div className={styles.filter}>
+              <div className={styles.filterSort}>
+                <span>Sort: </span>
+                <button onClick={handleAsc}>
+                  <AiOutlineSortAscending />
+                </button>
+              </div>
+              <div className={styles.searchImage}>
+                <Form.Control name="searchText" placeholder="Search..." className={styles.searchField} onChange={handleSearch} value={search} />
+                <Form.Select name="selectAmount" aria-label="Select Image" className={styles.selectValue} onChange={handleLimit} value={limit}>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                </Form.Select>
+              </div>
+            </div>
             {
               productList.map((data, idx) => (
                 <Col key={idx} lg={4} className=" mb-4">
@@ -44,6 +78,13 @@ export default function CategoryScreen() {
                 </Col>
               ))
             }
+            {/* {
+              product.map((data, idx) => (
+                <Col key={idx} lg={4} className=" mb-4">
+                  <ProductCard product={data} />
+                </Col>
+              ))
+            } */}
           </Row>
         </Container>
       </div>
